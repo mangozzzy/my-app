928ba35f586a643085479ed87b6c5acb0dacee55
맥북 <macbookpro@mango.local>
2024-11-19 23:38:41 +0900

    header footer add

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,4 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
 };
