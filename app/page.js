3cfbefc435c092e87b7235d4739c776f063de02e
맥북 <macbookpro@mango.local>
2024-11-19 23:51:16 +0900

    header component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import Header from "./component/Header";
 export default function Home() {
   return (
     <div>
-      <div>header</div>
+      <Header />
       <div>footer</div>
     </div>
 
