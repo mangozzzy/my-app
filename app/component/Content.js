ae319ffa64de0b3b1a2b05cd450cf0cb2a894c95
mangozzzy <mangozzzy@gmail.com>
2024-11-21 00:49:29 +0900

    add link

--- app/component/Content.js
+++ app/component/Content.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Content = () => {
   return (
     <div className="flex gap-5 p-10">
-        <Image src="/photo1.jpg" width={300} height={300} alt="옷사진"/>
-        <Image src="/photo2.jpg" width={300} height={300} alt="옷사진"/>
-        <Image src="/photo3.jpg" width={300} height={300} alt="옷사진"/>
-        <Image src="/photo4.jpg" width={300} height={300} alt="옷사진"/>
+        <Link href="/detail">
+            <Image src="/photo1.jpg" width={300} height={300} alt="옷사진"/>
+        </Link>
+        <Link href="/detail">
+            <Image src="/photo2.jpg" width={300} height={300} alt="옷사진"/>
+        </Link>
+        <Link href="/detail">
+            <Image src="/photo3.jpg" width={300} height={300} alt="옷사진"/>
+        </Link>
+        <Link href="/detail">
+            <Image src="/photo4.jpg" width={300} height={300} alt="옷사진"/>
+        </Link>
     </div>
   )
 };
